f1e1c0e7796903abb725e8216fc54f53573ecdc9
Emmanuele Bassi <ebassi@openedhand.com>
2008-03-02 10:51:09 +0000

    Port the empty string test to the GLib testing framework
    The old test-01 program tested the JsonParser with an empty string; instead
    of relying on the exit code and error messages on screen, it shoul use the
    new GTest API.
    
    This is the first test of the old test suite to be ported to the new test
    framework API.

--- tests/test-01.c
+++ tests/test-01.c
@@ -1,12 +1,18 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <glib.h>
+
 #include <json-glib/json-glib.h>
 
-static const gchar *test_empty = "";
+static const gchar *test_empty_string = "";
 
-int
-main (int argc, char *argv[])
+static void
+test_empty (void)
 {
   JsonParser *parser;
   GError *error = NULL;
@@ -14,18 +20,38 @@ main (int argc, char *argv[])
   g_type_init ();
 
   parser = json_parser_new ();
-  if (!json_parser_load_from_data (parser, test_empty, -1, &error))
+  g_assert (JSON_IS_PARSER (parser));
+
+  if (g_test_verbose ())
+    g_print ("checking json_parser_load_from_data with empty string...\n");
+
+  if (!json_parser_load_from_data (parser, test_empty_string, -1, &error))
     {
-      g_print ("Error: %s\n", error->message);
+      if (g_test_verbose ())
+        g_print ("Error: %s\n", error->message);
       g_error_free (error);
       g_object_unref (parser);
+      exit (1);
+    }
+  else
+    {
+      if (g_test_verbose ())
+        g_print ("checking json_parser_get_root...\n");
 
-      return EXIT_FAILURE;
+      g_assert (NULL == json_parser_get_root (parser));
     }
 
-  g_assert (NULL == json_parser_get_root (parser));
-
   g_object_unref (parser);
+}
 
-  return EXIT_SUCCESS;
+int
+main (int   argc,
+      char *argv[])
+{
+  g_type_init ();
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/json-glib/empty-string", test_empty);
+
+  return g_test_run ();
 }
