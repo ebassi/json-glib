cb5b3d5fa6244b0d20258203bd9df7e3148af57b
Emmanuele Bassi <ebassi@openedhand.com>
2008-03-02 10:50:17 +0000

    Add a JsonNode copy test unit
    The test unit copies a NULL JsonNode and checks that the copy and the
    original nodes are equivalent.

--- json-glib/tests/node-test.c
+++ json-glib/tests/node-test.c
@@ -3,6 +3,20 @@
 #include <string.h>
 
 static void
+test_copy (void)
+{
+  JsonNode *node = json_node_new (JSON_NODE_NULL);
+  JsonNode *copy = json_node_copy (node);
+
+  g_assert_cmpint (node->type, ==, copy->type);
+  g_assert_cmpint (json_node_get_value_type (node), ==, json_node_get_value_type (copy));
+  g_assert_cmpstr (json_node_type_name (node), ==, json_node_type_name (copy));
+
+  json_node_free (copy);
+  json_node_free (node);
+}
+
+static void
 test_null (void)
 {
   JsonNode *node = json_node_new (JSON_NODE_NULL);
@@ -22,6 +36,7 @@ main (int   argc,
   g_test_init (&argc, &argv, NULL);
 
   g_test_add_func ("/nodes/null-node", test_null);
+  g_test_add_func ("/nodes/copy-node", test_copy);
 
   return g_test_run ();
 }
