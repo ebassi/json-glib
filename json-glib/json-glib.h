b0daf7e3a2529dd9950d77a96a10233d68778f32
Emmanuele Bassi <ebassi@openedhand.com>
2008-04-24 14:54:05 +0100

    Include the newly added json-enum-types.h header
    When including json-glib/json-glib.h we get everything json-glib
    expose as a public symbol.

--- json-glib/json-glib.h
+++ json-glib/json-glib.h
@@ -6,5 +6,6 @@
 #include <json-glib/json-generator.h>
 #include <json-glib/json-parser.h>
 #include <json-glib/json-version.h>
+#include <json-glib/json-enum-types.h>
 
 #endif /* __JSON_GLIB_H__ */
