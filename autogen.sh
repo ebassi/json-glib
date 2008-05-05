4cb8a9078172c5f56ac5d56417b8656a7053df4a
Emmanuele Bassi <ebassi@openedhand.com>
2008-05-05 12:21:40 +0100

    Update autogen.sh
    Make autogen.sh test for the location of the current directory, and
    warn if the checkout is not complete.
    
    If gtk-doc is not installed (for instance, on an embedded minimal
    set up), do not fail and just provide a stub gtk-doc.make.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,26 @@
 #! /bin/sh
-gtkdocize || exit 1
-autoreconf -v --install || exit 1
-./configure "$@"
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PROJECT=JSON-GLib
+TEST_TYPE=-d
+FILE=json-glib
+
+test ${TEST_TYPE} ${FILE} || {
+        echo "You must run this script in the top-level ${PROJECT} directory"
+        exit 1
+}
+
+GTKDOCIZE=`which gtkdocize`
+if test -z $GTKDOCIZE; then
+        echo "*** No gtk-doc support ***"
+        echo "EXTRA_DIST =" > gtk-doc.make
+else
+        gtkdocize || exit $?
+fi
+
+autoreconf -v --install || exit $?
+
+./configure "$@" && echo "Now type 'make' to compile ${PROJECT}"
+
