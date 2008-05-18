81c02ef3db6901655f8a7117e5e2675d37096daf
Emmanuele Bassi <ebassi@openedhand.com>
2008-05-18 11:56:04 +0100

    Remove the include for json-scanner.h
    The json-scanner.h header file is not shipped with JSON-GLib anymore.

--- json-glib/json-glib.h
+++ json-glib/json-glib.h
@@ -2,7 +2,6 @@
 #define __JSON_GLIB_H__
 
 #include <json-glib/json-types.h>
-#include <json-glib/json-scanner.h>
 #include <json-glib/json-generator.h>
 #include <json-glib/json-parser.h>
 #include <json-glib/json-version.h>
