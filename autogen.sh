e583a9ec95f3d157091ec1e1a2ac8c493f0e23b2
Emmanuele Bassi <ebassi@linux.intel.com>
2009-04-13 22:04:10 +0100

    [build] Use gnome-autogen script
    When setting up the build environment we should use the tools that
    GNOME provides instead of calling autoreconf.

--- autogen.sh
+++ autogen.sh
@@ -3,24 +3,19 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PROJECT=JSON-GLib
+PKG_NAME=JSON-GLib
 TEST_TYPE=-d
 FILE=json-glib
 
 test ${TEST_TYPE} ${FILE} || {
-        echo "You must run this script in the top-level ${PROJECT} directory"
+        echo "You must run this script in the top-level ${PKG_NAME} directory"
         exit 1
 }
 
-GTKDOCIZE=`which gtkdocize`
-if test -z $GTKDOCIZE; then
-        echo "*** No gtk-doc support ***"
-        echo "EXTRA_DIST =" > gtk-doc.make
-else
-        gtkdocize || exit $?
-fi
+which gnome-autogen.sh || {
+        echo "*** You need to install gnome-common from GNOME SVN:"
+        echo "***  svn co http://svn.gnome.org/svn/gnome-common/trunk gnome-common"
+        exit 1
+}
 
-autoreconf -v --install || exit $?
-
-./configure "$@" && echo "Now type 'make' to compile ${PROJECT}"
-
+REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
