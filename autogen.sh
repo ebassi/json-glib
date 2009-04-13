e2c65a75d68aafa26f2084928e732961e48beb99
Emmanuele Bassi <ebassi@linux.intel.com>
2009-04-13 23:46:52 +0100

    [build] Improve the build system
    Clean up the configure.ac and Makefile.am files to use something
    that is not entirely made of FAIL.
    
    Also, use Shave to sanitize the libtool and compiler incantations
    and restore sanity to the build system.

--- autogen.sh
+++ autogen.sh
@@ -19,3 +19,12 @@ which gnome-autogen.sh || {
 }
 
 REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+
+# we need to patch gtk-doc.make to support pretty output with
+# libtool 1.x.  Should be fixed in the next version of gtk-doc.
+# To be more resilient with the various versions of gtk-doc one
+# can find, just sed gkt-doc.make rather than patch it.
+sed -e 's#) --mode=compile#) --tag=CC --mode=compile#' gtk-doc.make > gtk-doc.temp \
+                && mv gtk-doc.temp gtk-doc.make
+sed -e 's#) --mode=link#) --tag=CC --mode=link#' gtk-doc.make > gtk-doc.temp \
+                && mv gtk-doc.temp gtk-doc.make
