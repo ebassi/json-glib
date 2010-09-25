ebassi/json-glib

commit 2d7550948dfb2e5907b851bc2c4bd296a7526086

gobject: Use construct and construct-only properties
Right now, we're checking twice for G_PARAM_CONSTRUCT_ONLY, but what we
really want is to check for both G_PARAM_CONSTRUCT and
G_PARAM_CONSTRUCT_ONLY properties when creating a new instance from a
JSON definition.

json-glib/json-gobject.c
@@ -216,7 +216,7 @@ json_gobject_new (GType       gtype,
         goto next_member;
 
       if (!(pspec->flags & G_PARAM_CONSTRUCT_ONLY) ||
-          !(pspec->flags & G_PARAM_CONSTRUCT_ONLY))
+          !(pspec->flags & G_PARAM_CONSTRUCT))
         goto next_member;
 
       if (!(pspec->flags & G_PARAM_WRITABLE))

commit 9b6acd68e156a45cc845bdebd99c174e82d0677c

gobject: Fix deserialization of construct-only properties
Commit 2d7550948dfb2e5907b851bc2c4bd296a7526086 broke the construct-only
properties; we now only check for the G_PARAM_CONSTRUCT_ONLY flag, and
pass construct-only properties to g_object_newv(); all the properties
flagged as G_PARAM_CONSTRUCT gets passed with the rest of the properties
after that.

json-glib/json-gobject.c
@@ -216,8 +216,8 @@ json_gobject_new (GType       gtype,
       if (!pspec)
         goto next_member;
 
-      if (!(pspec->flags & G_PARAM_CONSTRUCT_ONLY) ||
-          !(pspec->flags & G_PARAM_CONSTRUCT))
+      /* we only apply construct-only properties here */
+      if ((pspec->flags & G_PARAM_CONSTRUCT_ONLY) == 0)
         goto next_member;
 
       if (!(pspec->flags & G_PARAM_WRITABLE))