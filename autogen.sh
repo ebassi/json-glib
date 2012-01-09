424244569257c280c1926deadf266aa564b795ff
Ryan Lortie <desrt@desrt.ca>
2012-01-09 16:15:06 +0100

    autogen.sh: remove out-of-tree check
    Don't verify from autogen.sh that we are in the source tree -- this
    breaks out-of-tree builds in a rather direct way.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=667454

--- autogen.sh
+++ autogen.sh
@@ -4,13 +4,6 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME=JSON-GLib
-TEST_TYPE=-d
-FILE=json-glib
-
-test ${TEST_TYPE} ${FILE} || {
-        echo "You must run this script in the top-level ${PKG_NAME} directory"
-        exit 1
-}
 
 which gnome-autogen.sh || {
         echo "*** You need to install gnome-common from GNOME Git:"
