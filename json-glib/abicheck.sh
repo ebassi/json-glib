0abf343608dd5833a9d468ac29a81f8e03e1dc0e
Emmanuele Bassi <ebassi@gnome.org>
2013-12-04 12:55:11 +0000

    Make abicheck output TAP directives
    So that we can use it with the TAP driver like the rest of the tests.

--- json-glib/abicheck.sh
+++ json-glib/abicheck.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 
-cpp -P ${cppargs} ${srcdir:-.}/json-glib.symbols | sed -e '/^$/d' -e 's/ G_GNUC.*$//' -e 's/ PRIVATE//' -e 's/ DATA//' | sort > expected-abi
+echo "1..1"
+echo "# Start of abicheck tests"
+
+cpp -P ${cppargs} ${srcdir:-.}/json-glib/json-glib.symbols | sed -e '/^$/d' -e 's/ G_GNUC.*$//' -e 's/ PRIVATE//' -e 's/ DATA//' | sort > expected-abi
 
 nm -D -g --defined-only .libs/libjson-glib-1.0.so | cut -d ' ' -f 3 | egrep -v '^(__bss_start|_edata|_end)' | sort > actual-abi
-diff -u expected-abi actual-abi && rm -f expected-abi actual-abi
+diff -u expected-abi actual-abi && rm -f expected-abi actual-abi && echo "ok 1 expected-abi"
+
+echo "# End of abicheck tests"
